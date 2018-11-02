fe54154311ab4ef95b17b6f4980eb878f775bfe1
Dominik Schreiber <mail@dominikschreiber.de>
2018-11-02 14:22:19 +0100

    Expand and adjust test cases

--- src/test/java/edu/kit/aquaplanning/aquaplanning/TestPlanners.java
+++ src/test/java/edu/kit/aquaplanning/aquaplanning/TestPlanners.java
@@ -1,7 +1,11 @@
 package edu.kit.aquaplanning.aquaplanning;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import edu.kit.aquaplanning.grounding.FullEnumerationGrounder;
 import edu.kit.aquaplanning.grounding.Grounder;
 import edu.kit.aquaplanning.grounding.RelaxedPlanningGraphGrounder;
 import edu.kit.aquaplanning.model.ground.GroundPlanningProblem;
@@ -9,29 +13,58 @@ import edu.kit.aquaplanning.model.ground.Plan;
 import edu.kit.aquaplanning.model.lifted.PlanningProblem;
 import edu.kit.aquaplanning.parsing.ProblemParser;
 import edu.kit.aquaplanning.planners.DefaultPlanner;
-import edu.kit.aquaplanning.planners.Planner;
 import edu.kit.aquaplanning.validate.Validator;
 import junit.framework.TestCase;
 
 public class TestPlanners extends TestCase {
-	
-	public void testDefaultPlannerOnTestfiles() throws IOException {
-		Planner planner = new DefaultPlanner();
 		
-		testPlannerOnProblem(planner, new ProblemParser().parse("testfiles/childsnack/domain.pddl", "testfiles/childsnack/p01.pddl"));
-		testPlannerOnProblem(planner, new ProblemParser().parse("testfiles/gripper/domain.pddl", "testfiles/gripper/p01.pddl"));
-		testPlannerOnProblem(planner, new ProblemParser().parse("testfiles/rover/domain.pddl", "testfiles/rover/p01.pddl"));
-		testPlannerOnProblem(planner, new ProblemParser().parse("testfiles/TM/domain.pddl", "testfiles/TM/p_det.pddl"));
-		testPlannerOnProblem(planner, new ProblemParser().parse("testfiles/RPG/domain.pddl", "testfiles/RPG/p01.pddl"));
+	public void testQuantifications() throws FileNotFoundException, IOException {
+
+		fullTest("testfiles/quantifications/domain.pddl", "testfiles/quantifications/p1.pddl");
+		fullTest("testfiles/quantifications/domain.pddl", "testfiles/quantifications/p2.pddl");
 	}
 	
-	private void testPlannerOnProblem(Planner planner, PlanningProblem problem) {
+	public void testDefaultDomains() throws FileNotFoundException, IOException {
 		
-		Grounder grounder = new RelaxedPlanningGraphGrounder();
-		GroundPlanningProblem planningProblem = grounder.ground(problem);
-		Plan plan = planner.findPlan(planningProblem);
-		System.out.println(plan);
-		assertTrue(Validator.planIsValid(planningProblem, plan));
+		String[] domains = {"rover", "childsnack", "gripper"};
+		for (String domain : domains) {
+			fullTest("testfiles/" + domain + "/domain.pddl", "testfiles/" + domain + "/p01.pddl");
+		}
+	}
+	
+	public void testCustomDomains() throws FileNotFoundException, IOException {
+
+		fullTest("testfiles/RPG/domain.pddl", "testfiles/RPG/p01.pddl");
+		fullTest("testfiles/TM/domain.pddl", "testfiles/TM/p_det.pddl");
+	}
+	
+	private void fullTest(String domainFile, String problemFile) throws FileNotFoundException, IOException {
+		
+		System.out.println("Testing domain \"" + domainFile + "\", problem \"" + problemFile + "\".");
+		
+		List<Grounder> grounders = new ArrayList<>();
+		grounders.add(new FullEnumerationGrounder());
+		grounders.add(new RelaxedPlanningGraphGrounder());
+				
+		System.out.println("Parsing ...");
+		PlanningProblem pp = new ProblemParser().parse(domainFile, problemFile);
+		
+		for (Grounder grounder : grounders) {
+			
+			System.out.println("Grounding with " 
+					+ grounder.getClass().getSimpleName() + "...");
+			GroundPlanningProblem gpp = grounder.ground(pp);
+			
+			System.out.println("Planning ...");
+			Plan plan = new DefaultPlanner().findPlan(gpp);
+			
+			assertTrue(plan != null && plan.getLength() > 0);
+			assertTrue(Validator.planIsValid(gpp, plan));
+			
+			System.out.println("Done.");
+		}
+		
+		System.out.println();
 	}
 
 }
