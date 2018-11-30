46c4c295360043dcc577b098cb8773c22d8ea3cf
biotomas <tomas.balyo@kit.edu>
2018-11-30 17:15:30 +0100

    fixed typos in comments

--- src/main/java/edu/kit/aquaplanning/planners/SimpleSatPlanner.java
+++ src/main/java/edu/kit/aquaplanning/planners/SimpleSatPlanner.java
@@ -151,7 +151,7 @@ public class SimpleSatPlanner implements Planner {
 	}
 
 	/**
-	 * Clauses about about the transition between step and step+1
+	 * Clauses encoding the transition between step and step+1
 	 * @param problem
 	 * @param solver
 	 * @param step
@@ -172,7 +172,7 @@ public class SimpleSatPlanner implements Planner {
 			}
 		}
 		
-		// frame axioms -- if an atom changes there must and action causing it
+		// frame axioms -- if an atom changes then there must be an action causing it
 		for (int atomId = 0; atomId < problem.getNumAtoms(); atomId++) {
 			// change of atom from true to false
 			List<Action> supports = getSupportingActions(atomId, false);
@@ -186,7 +186,7 @@ public class SimpleSatPlanner implements Planner {
 			}
 			solver.addClause(p2n);
 
-			// change of atom form false to true
+			// change of atom from false to true
 			supports = getSupportingActions(atomId, true);
 			int[] n2p = new int[2+getSupportingActions(atomId, true).size()];
 			n2p[0] = getAtomSatVariable(atomId, step);
